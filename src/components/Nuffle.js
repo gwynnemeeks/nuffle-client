3d170d9b1d1f4736ff1cd5e7edbb1ab45c276406
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-09 10:31:02 -0600

    setup complete

---
+++ src/components/Nuffle.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import './Nuffle.scss';
+
+export const Nuffle = () => (
+    <>
+    <h1>Nuffle</h1>
+    <address>
+        <div>Blood Bowl:</div>
+        <div>The violent fantasy football game of your nightmares</div>
+    </address>
+    </>
+);
