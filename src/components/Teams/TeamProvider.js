58f1215758c34efe6d7f01db04a15f6520953af3
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-16 15:56:27 -0600

    wip with problems

---
+++ src/components/Teams/TeamProvider.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react"
+
+export const TeamContext = React.createContext()
+
+export const TeamProvider = (props) => {
+    const [teams, setTeams] = useState([])
+
+    const getTeams = () => {
+        return fetch("http://localhost:8000/teams",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("token")}`
+        }
+    })
+        .then(response => response.json())
+        .then(setTeams)
+    }
+
+    const createTeam = team => {
+        return fetch("http://localhost:8000/teams", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify(team)
+        })
+        .then(response => response.json())
+        .then(getTeams)
+    }
+}
