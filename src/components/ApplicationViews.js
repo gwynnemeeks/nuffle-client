8b826ddc606deee18335f81f92f8490c0a87ea06
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 14:41:38 -0600

    wip app views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,19 +1,36 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { ProfileProvider } from "./Auth/AuthProvider"
-import { Profile } from "./Auth/Profile"
+// import { ProfileProvider } from "./Auth/AuthProvider"
+// import { Profile } from "./Auth/Profile"
 
-export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            lineHeight: "1.75rem"
-        }}>
-            <ProfileProvider>
-                <Route exact path="/profile">
-                    <Profile />
+import { TeamProvider } from "./Teams/TeamProvider"
+import { PlayerProvider } from "./Players/PlayerProvider"
+import { EventProvider } from "./Events/EventProvider"
+
+import { TeamList } from "./Teams/TeamList"
+import { PlayerList } from "./Players/PlayerList"
+import { EventList } from "./Events/EventList"
+
+export const ApplicationViews = (props) => {
+    return (
+        <>
+            <TeamProvider>
+                <Route exact path="/">
+                    <TeamList />
                 </Route>
-            </ProfileProvider>
-        </main>
+            </TeamProvider>
+
+            <PlayerProvider>
+                <Route path="/players">
+                    <PlayerList />
+                </Route>
+            </PlayerProvider>
+
+            <EventProvider>
+                <Route path="/events">
+                    <EventList />
+                </Route>
+            </EventProvider>
         </>
+    )
 }
