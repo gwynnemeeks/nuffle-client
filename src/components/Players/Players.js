9144cb4287139ace1ee00c2803c4d9343d36855f
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 11:25:52 -0600

    wip card data

---
+++ src/components/Players/Players.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Players.scss"
+
+export const Player = ({ player }) => (
+    <section className="player">
+        <h3 className="player__name">{player.name}</h3>
+        <p className="player__position">{player.position}</p>
+        <p className="player__history">{player.history}</p>
+    </section>
+)
