e4c5e3ac4e19f2daebbb645ee1a4d17dee0558a4
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 11:16:54 -0600

    wip provider

---
+++ src/components/Players/PlayerProvider.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react"
+
+export const PlayerContext = React.createContext()
+
+export const PlayerProvider = (props) => {
+    const [players, setPlayers] = useState([])
+
+    const getPlayers = () => {
+        return fetch("http://localhost:8000/players", {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("token")}`
+            }
+    })
+        .then(response => response.json())
+        .then(setPlayers)
+    }
+
+    const createPlayer = player => {
+        return fetch("http://localhost:8000/players", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify(player)
+        })
+        .then(response => response.json())
+        .then(getPlayers)
+    }
+
+    return (
+        <PlayerContext.Provider value={{
+            players, getPlayers, createPlayer
+        }}>
+            {props.children}
+        </PlayerContext.Provider>
+    )
+}
