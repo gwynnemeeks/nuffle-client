01e8aa395992a995cb47c72e1a2d4730a72701c3
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 14:33:19 -0600

    wip nav

---
+++ src/components/Nav/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.scss"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/">Nuffle: Blood Bowl</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/teams">Teams</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/players">Players</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/events">Events</Link>
+            </li>
+        </ul>
+    )
+}
