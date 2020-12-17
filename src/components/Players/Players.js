d6f9638f6f8d7ccb9335e8165036ec9350041c56
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 16:01:53 -0600

    wip cards

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -2,9 +2,18 @@ import React from "react"
 import "./Players.scss"
 
 export const Player = ({ player }) => (
-    <section className="player">
-        <h3 className="player__name">{player.name}</h3>
-        <p className="player__position">{player.position}</p>
-        <p className="playes__history">{player.history}</p>
+    <section classNameName="player">
+        <div className="card text-center">
+    <div className="card-body">
+    <h5 className="card-title">{player.name}</h5>
+    <p className="card-text">{player.position}</p>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">AG: {player.agility}</li>
+    <li className="list-group-item">AV: {player.armor_value}</li>
+    <li className="list-group-item">ST: {player.strength}</li>
+    <li className="list-group-item">MV: {player.movement}</li>
+  </ul>
+    </div>
     </section>
 )
--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -3,7 +3,12 @@ import "./Teams.scss"
 
 export const Team = ({ team }) => (
     <section className="team">
-        <h3 className="team__team_name">{team.team_name}</h3>
-        <p className="team__team_value">{team.team_value}</p>
+        <div class="card w-50">
+        <div class="card-body">
+        <h5 class="card-title">{team.team_name}</h5>
+    <p class="card-text">{team.team_value}</p>
+    <button class="btn btn-primary">Team Roster</button>
+    </div>
+    </div>
     </section>
 )
