0556125430e05b59fe7521884de9b0eb92a141b1
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 14:02:29 -0600

    wip nuffle

--- src/components/Events/EventProvider.js
+++ src/components/Events/EventProvider.js
@@ -3,10 +3,10 @@ import React, { useState } from "react"
 export const EventContext = React.createContext()
 
 export const EventProvider = (props) => {
-    const [events, setEvents] = useState([{name: ''}])
+    const [events, setEvents] = useState([])
 
     const getEvents = () => {
-        return fetch("http://localhost:8000/Events", {
+        return fetch("http://localhost:8000/events", {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("token")}`
             }
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,9 +1,8 @@
 import React from "react"
 import "./Events.scss"
 
-export const event = ({ event }) => (
+export const Event = ({ event }) => (
     <section className="event">
         <h3 className="event__location">{event.location}</h3>
-        <p className="event__event_schedule">{event.event_schedule}</p>
     </section>
 )
