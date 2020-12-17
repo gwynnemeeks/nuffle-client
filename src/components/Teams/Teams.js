d3128a7ac0b99b95b8e1f915b457af1c99711bd4
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-17 10:22:32 -0600

    wip

---
+++ src/components/Teams/Teams.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Teams.scss"
+
+export const Team = () => (
+    <section className="team">
+        <h3 className="team__name">Your Teams</h3>
+    </section>
+)
