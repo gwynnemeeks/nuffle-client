7e0a6e8a9c66e3c947645b0316964ae509e0220b
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-18 13:33:39 -0600

    wip league list

---
+++ src/components/Leagues/LeagueList.js
@@ -0,0 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from "react"
+import { LeagueContext } from "./LeagueProvider"
+import { League } from "./Leagues"
+import "./Leagues.scss"
+
+export const LeagueList = () => {
+    const { leagues, getLeagues } = useContext(LeagueContext)
+
+    useEffect(() => {
+        getLeagues()
+    }, [])
+
+    return(
+        <div className="leagues">
+            {
+                leagues.map(lea => <League key={lea.id} league={lea} />)
+            }
+        </div>
+    )
+}
