daf9a62a2c9e23d751cb9d1fb1a5accf757f7190
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-22 09:15:11 -0800

    wip button

--- src/components/Events/EventList.js
+++ src/components/Events/EventList.js
@@ -4,7 +4,7 @@ import { EventContext } from "./EventProvider"
 import { Event } from "./Events"
 import "./Events.scss"
 
-export const EventList = () => {
+export const EventList = (props) => {
     const { events, getEvents } = useContext(EventContext)
 
     useEffect(() => {
@@ -13,6 +13,12 @@ export const EventList = () => {
 
     return(
         <div className="events">
+            <h1>Your Upcoming Events</h1>
+            <button className="btn btn-light"
+            onClick={() => {
+            props.history.push({ pathname: "/eventss/new" })
+            }}
+            >Register New Event</button>
             {
                 events.map(eve => <Event key={eve.id} event={eve} />)
             }
