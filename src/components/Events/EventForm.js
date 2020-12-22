3904f06ba6f0769ef0c68ff3a1105371debb9395
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-22 09:05:24 -0800

    wip form

--- src/components/Events/EventForm.js
+++ src/components/Events/EventForm.js
@@ -30,6 +30,60 @@ export const EventForm = props => {
                 />
             </div>
         </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="time">Event Time: </label>
+                <input type="text" name="time" required autoFocus className="form-control"
+                    value={currentEvent.time}
+                    onChange={handleControlledInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="location">Event Location: </label>
+                <input type="text" name="location" required autoFocus className="form-control"
+                    value={currentEvent.location}
+                    onChange={handleControlledInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="finalScore">Event Final Score: </label>
+                <input type="text" name="finalScore" required autoFocus className="form-control"
+                    value={currentEvent.finalScore}
+                    onChange={handleControlledInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="eventSchedule">Event Event Schedule: </label>
+                <input type="text" name="eventSchedule" required autoFocus className="form-control"
+                    value={currentEvent.eventSchedule}
+                    onChange={handleControlledInputChange}
+                />
+            </div>
+        </fieldset>
+        <button type="submit"
+                onClick={evt => {
+                    // Prevent form from being submitted
+                    evt.preventDefault()
+
+                    const event = {
+                        day: currentEvent.day,
+                        time: currentEvent.time,
+                        location: currentEvent.location,
+                        finalScore: currentEvent.finalScore,
+                        eventSchedule: currentEvent.eventSchedule                        
+                    }
+
+                    // Send POST request to your API
+                    createEvent(event)
+                    props.history.push("/events")
+                }}
+                className="btn btn-primary">Create</button>
 
         </form>
     )
