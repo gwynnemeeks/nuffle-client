bc30eeffd7c9e0d0a15db7e70b98ed75d02eb754
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-01-02 11:37:09 -0600

    wip

--- src/components/Teams/TeamDetail.js
+++ src/components/Teams/TeamDetail.js
@@ -9,6 +9,7 @@ export const TeamDetails = (props) => {
     const { teams, getSingleTeam } = useContext(TeamContext)
 
     const [setTeam] = useState()
+    // {coach: {}, league: {}}
 
     useEffect(() => {
         const teamId = parseInt(props.match.params.teamId)
@@ -18,9 +19,11 @@ export const TeamDetails = (props) => {
 
     return (
         <section className="team">
-            <h3 className="team__team_name">{teams.team_name}</h3>
+            {/* <h2 className="team__league_name">{teams.league.league_name}</h2> */}
+            <h3 className="team__team_name">Team: {teams.team_name}</h3>
+            {/* <h4 className="team__coach">Coach: {teams.coach.title}</h4> */}
             <p className="team__team_type">Race: {teams.team_type}</p>
-            <p className="team__team_value">Value: {teams.team_value}</p>
+            <p className="team__team_value">Valued at: {teams.team_value} GP</p>
             
         </section>
     )
