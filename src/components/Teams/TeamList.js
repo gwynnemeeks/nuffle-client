5facc5d3fffe4566b95339fc6e0a41df0ba993f9
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-01-11 10:25:52 -0600

    wip team style

--- src/components/Teams/TeamList.js
+++ src/components/Teams/TeamList.js
@@ -12,25 +12,33 @@ export const TeamList = (props) => {
     }, [])
 
     return (
-        <div className="teams">
-            <div className="col-6 offset-3">
-                <h1>Your Teams</h1>
-                <button className="btn btn-light"
-                    onClick={() => {
-                        props.history.push({ pathname: "/teams/new" })
-                    }}
-                >Register New Team</button>
-
+        <>
+            <div className="teams">
                 <article className="teamList">
-                    {
-                        teams.map(tea => {
-                            return <Link key={tea.id} to={`/teams/${tea.id}`}>
-                                <h3>{tea.team_name}</h3>
-                            </Link>
-                        })
-                    }
+                    <div className="card mb-3">
+                        <div className="col-md-4">
+                            <h3>{teams.coach.title}</h3>
+                            <button className="btn btn-light"
+                                onClick={() => {
+                                    props.history.push({ pathname: "/teams/new" })
+                                }}
+                            >Register New Team</button>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h1>Your Teams</h1>
+                                {
+                                    teams.map(tea => {
+                                        return <Link key={tea.id} to={`/teams/${tea.id}`}>
+                                            <h3>{tea.team_name}</h3>
+                                        </Link>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
                 </article>
-            </div>
-        </div>
+                </div>
+        </>
     )
 }
